54cf36723bc2bc5a8acd82baf05535e426ba030a
Mauricio Fontana <twittmfontana@gmail.com>
2020-09-17 22:01:08 -0300

    optimizing background drawing

--- lib/widgets/IntraContainer.dart
+++ lib/widgets/IntraContainer.dart
@@ -1,5 +1,3 @@
-
-
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -8,9 +6,8 @@ import 'package:intra/meta/intraSpacing/margin.dart';
 import 'package:intra/theme.dart';
 
 class IntraContainer extends StatelessWidget {
-
   final Widget child;
-  
+
   IntraContainer(this.child);
 
   @override
@@ -20,7 +17,10 @@ class IntraContainer extends StatelessWidget {
       child: Container(
         decoration: new BoxDecoration(color: white3),
         child: CustomPaint(
-          painter: DottedBackground(),
+          painter: DottedBackground(
+            MediaQuery.of(context).size.width,
+            MediaQuery.of(context).size.height,
+          ),
           child: Container(
             child: SafeArea(
               child: Container(
@@ -28,8 +28,7 @@ class IntraContainer extends StatelessWidget {
                     IntraMargin.horizontalMargin,
                     IntraMargin.verticalMargin,
                     IntraMargin.horizontalMargin,
-                    IntraMargin.verticalMargin
-                ),
+                    IntraMargin.verticalMargin),
                 child: child,
               ),
             ),
@@ -40,31 +39,31 @@ class IntraContainer extends StatelessWidget {
   }
 }
 
-
 class DottedBackground extends CustomPainter {
-
   static final offsets = [];
   static final brush = Paint()
-    ..color =Color(0xFFBBBBBB)
+    ..color = Color(0xFFBBBBBB)
     ..strokeCap = StrokeCap.round
     ..strokeWidth = 2;
+
+  final double screenWidth;
+  final double screenHeight;
+
+  DottedBackground(this.screenWidth, this.screenHeight);
 
   @override
   void paint(Canvas canvas, Size size) {
     List<Offset> offsets = List();
 
-    // var finalprint = '';
-
-    for (var i = 0; i < 27; i++) {
-      for (var j = 0; j < 50; j++) {
-        offsets.add(Offset(i*15.0, j*15.0));
-        // finalprint += "Offset(${i*15.0}, ${j*15.0}), ";
+    for (var i = 0; i < screenWidth; i += 15) {
+      for (var j = 0; j < screenHeight; j += 15) {
+        offsets.add(Offset(i + .0, j + .0));
       }
-      // finalprint += "\n";
     }
 
     canvas.drawPoints(PointMode.points, offsets, brush);
   }
 
-  @override bool shouldRepaint(CustomPainter oldDelegate) => false;
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
