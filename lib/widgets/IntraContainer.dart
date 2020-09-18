598d0bb27bef373612a2365ea8a326c062e1c483
Mauricio Fontana <twittmfontana@gmail.com>
2020-09-17 21:53:50 -0300

    adding dotted background that still needs some work

--- lib/widgets/IntraContainer.dart
+++ lib/widgets/IntraContainer.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intra/meta/intraSpacing/margin.dart';
@@ -17,18 +19,52 @@ class IntraContainer extends StatelessWidget {
       type: MaterialType.transparency,
       child: Container(
         decoration: new BoxDecoration(color: white3),
-        child: SafeArea(
+        child: CustomPaint(
+          painter: DottedBackground(),
           child: Container(
-            padding: EdgeInsets.fromLTRB(
-                IntraMargin.horizontalMargin,
-                IntraMargin.verticalMargin,
-                IntraMargin.horizontalMargin,
-                IntraMargin.verticalMargin
+            child: SafeArea(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(
+                    IntraMargin.horizontalMargin,
+                    IntraMargin.verticalMargin,
+                    IntraMargin.horizontalMargin,
+                    IntraMargin.verticalMargin
+                ),
+                child: child,
+              ),
             ),
-            child: child,
           ),
         ),
       ),
     );
   }
+}
+
+
+class DottedBackground extends CustomPainter {
+
+  static final offsets = [];
+  static final brush = Paint()
+    ..color =Color(0xFFBBBBBB)
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 2;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    List<Offset> offsets = List();
+
+    // var finalprint = '';
+
+    for (var i = 0; i < 27; i++) {
+      for (var j = 0; j < 50; j++) {
+        offsets.add(Offset(i*15.0, j*15.0));
+        // finalprint += "Offset(${i*15.0}, ${j*15.0}), ";
+      }
+      // finalprint += "\n";
+    }
+
+    canvas.drawPoints(PointMode.points, offsets, brush);
+  }
+
+  @override bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
